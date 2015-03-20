c9fab529c150dacedc55ce2f543c1acb94897e5c
Muscovado Labs <muscovado.kn@gmail.com>
2015-03-20 12:12:34 -0400

    Create shouter.h

---
+++ shouter.h
@@ -0,0 +1,20 @@
+//shouter.h
+
+#include<iostream>
+using std::cout;
+using std::endl;
+
+//function to move screen down 25 lines
+void shouter()
+{	cout 	<< endl << endl<< endl << endl << endl
+		 << endl << endl<< endl << endl << endl
+		 << endl << endl<< endl << endl << endl
+		 << endl << endl<< endl << endl << endl
+		 << endl << endl<< endl << endl << endl;
+}//end shouter()
+
+//end shouter.h
+
+//	FITZROY EDINBOROUGH		//
+//	408004140				//
+//	Cre 16.03.09				//
