94989febae4fa1839f6840e544210aa970e04f56
Muscovado Labs <muscovado.kn@gmail.com>
2015-03-20 12:16:39 -0400

    Create create_gameBoard.h

---
+++ create_gameBoard.h
@@ -0,0 +1,12 @@
+// create_gameBoard.h
+
+gameBoard create_gameBoard()
+{	gameBoard G(1);//create gameBoard object, integer argument inconsequential
+	return G;
+}//end create_gameBoard()
+
+//end create_gameBoard.h
+
+//	FITZROY EDINBOROUGH		//
+//	408004140				//
+//	Cre 15.03.09				// 
