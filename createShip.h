9bf1d0814041d6d5973b461d363bad6a97dc3e04
Muscovado Labs <muscovado.kn@gmail.com>
2015-03-20 12:16:59 -0400

    Create createShip.h

---
+++ createShip.h
@@ -0,0 +1,10 @@
+//createShip.h
+
+Ship createShip(string type, int bow, int length)
+{	Ship S(type, bow, length);
+	return S;
+}//end createShip()
+
+//	FITZROY EDINBOROUGH		//
+//	408004140				//
+//	Cre 15.03.09				// 
